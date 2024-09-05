4616cbf63ef65550f5f07591b7b2fa5128e893be
김현진 <summersad10@gmail.com>
2024-09-05 17:29:48 +0900

    I/O

--- object/src/io/SerialEx2.java
+++ object/src/io/SerialEx2.java
@@ -6,16 +6,14 @@ import java.io.ObjectInputStream;
 
 public class SerialEx2 {
     public static void main(String[] args) {
-        Person p1 = new Person("홍길동", "엔지니어", "hong@javamail.net");
-        Person p2 = new Person("성춘향", "교사", "sung@javamail.net");
 
         try (FileInputStream fis = new FileInputStream("c:\\temp\\person.dat");
                 BufferedInputStream bis = new BufferedInputStream(fis);
                 ObjectInputStream ois = new ObjectInputStream(bis)) {
 
             for (int i = 0; i < 2; i++) {
-                // Person pi = (Person) ois.readObject();
-                // System.out.println(pi);
+                Person p1 = (Person) ois.readObject();
+                System.out.println(p1);
             }
 
         } catch (Exception e) {
